44eedfe51b3256e1f23b5fe804dab4ae44aec606
TangledTessellations <mcevoya@oregonstate.edu>
2019-01-11 19:32:58 -0800

    Update migrations to include answers column in Questions

--- Backend/mig/20181128130441_questions.js
+++ Backend/mig/20181128130441_questions.js
@@ -13,6 +13,7 @@ exports.up = function(knex, Promise) {
         table.text("question").notNullable();
         table.string("correct_answer").notNullable();
         table.string("incorrect_answers").notNullable();
+        table.string("answers").notNullable();
     });
 };
 
